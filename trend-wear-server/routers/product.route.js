6cb72c25634256b270be62d68b7c5f8882e8476e
Ahmad Ali <aali.dev7@gmail.com>
2025-01-17 16:07:03 +0530

    created addProduct endpoint

---
+++ trend-wear-server/routers/product.route.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const ProductController = require("../controller/product.controller");
+
+router.post("/addProduct", ProductController.addProduct);
+
+module.exports = router;
